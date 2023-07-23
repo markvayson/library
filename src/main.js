d411fddfe4acd779a170084b5c60f2745b991a93
markvayson <markvayson@gmail.com>
2023-07-23 17:45:21 +0400

    added html,css,js files

---
+++ src/main.js
@@ -0,0 +1,19 @@
+let myLibrary = [
+  {
+    bookName: "Atomic Havits",
+    author: "James Clear",
+    bookPublished: "October 16, 2018",
+    language: "English",
+    publisher: "Penguin Publishing Group",
+    pageCount: 320,
+    shortInfo: "An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+  },
+];
+
+function Book() {
+  console.log("hello");
+}
+
+window.onload = () => {
+  Book();
+};
