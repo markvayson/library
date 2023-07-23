3ea1f30bdbb246fb195fe6dc16b5ef317f9c173b
markvayson <markvayson@gmail.com>
2023-07-23 21:47:41 +0400

    finale

---
+++ src/books.js
@@ -0,0 +1,131 @@
+let myLibrary = [
+  {
+    bookName: "Atomic Habits",
+    author: "James Clear",
+    bookPublished: "October 16, 2018",
+    language: "English",
+    publisher: "Penguin Publishing Group",
+    pageCount: 320,
+    shortInfo: "An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+  },
+  {
+    bookName: "Learning How to Learn",
+    author: "Barbara Oakley, PhD",
+    bookPublished: "August 7, 2018",
+    language: "English",
+    publisher: "Penguin Publishing Group",
+    pageCount: 256,
+    shortInfo:
+      "How to Succeed in School Without Spending All Your Time Studying; A Guide for Kids and Teens",
+  },
+  {
+    bookName: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    bookPublished: "April 10, 1925",
+    language: "English",
+    publisher: "Charles Scribner's Sons",
+    pageCount: 180,
+    shortInfo:
+      "A classic novel exploring the decadence and moral decline of the Jazz Age.",
+  },
+  {
+    bookName: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    bookPublished: "July 11, 1960",
+    language: "English",
+    publisher: "J. B. Lippincott & Co.",
+    pageCount: 324,
+    shortInfo:
+      "A Pulitzer Prize-winning novel that addresses racial injustice and moral growth in the American South.",
+  },
+  {
+    bookName: "1984",
+    author: "George Orwell",
+    bookPublished: "June 8, 1949",
+    language: "English",
+    publisher: "Secker & Warburg",
+    pageCount: 328,
+    shortInfo:
+      "A dystopian novel portraying a totalitarian society and the consequences of government overreach.",
+  },
+  {
+    bookName: "Pride and Prejudice",
+    author: "Jane Austen",
+    bookPublished: "January 28, 1813",
+    language: "English",
+    publisher: "T. Egerton, Whitehall",
+    pageCount: 432,
+    shortInfo:
+      "A romantic novel following the lives of the Bennet sisters and their pursuit of love and happiness.",
+  },
+  {
+    bookName: "The Lord of the Rings",
+    author: "J.R.R. Tolkien",
+    bookPublished: "July 29, 1954",
+    language: "English",
+    publisher: "Allen & Unwin",
+    pageCount: 1178,
+    shortInfo:
+      "An epic fantasy trilogy chronicling the quest to destroy the One Ring and defeat the dark lord Sauron.",
+  },
+  {
+    bookName: "To the Lighthouse",
+    author: "Virginia Woolf",
+    bookPublished: "May 5, 1927",
+    language: "English",
+    publisher: "Hogarth Press",
+    pageCount: 209,
+    shortInfo:
+      "A novel exploring the complexities of human relationships and the passage of time through the Ramsay family.",
+  },
+  {
+    bookName: "One Hundred Years of Solitude",
+    author: "Gabriel García Márquez",
+    bookPublished: "May 30, 1967",
+    language: "Spanish",
+    publisher: "Harper & Row",
+    pageCount: 417,
+    shortInfo:
+      "A magical realist novel tracing the multi-generational story of the Buendía family in the fictional town of Macondo.",
+  },
+  {
+    bookName: "Brave New World",
+    author: "Aldous Huxley",
+    bookPublished: "February 2, 1932",
+    language: "English",
+    publisher: "Chatto & Windus",
+    pageCount: 311,
+    shortInfo:
+      "A dystopian novel portraying a society organized on the principles of mass production, pleasure, and conformity.",
+  },
+  {
+    bookName: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    bookPublished: "July 16, 1951",
+    language: "English",
+    publisher: "Little, Brown and Company",
+    pageCount: 277,
+    shortInfo:
+      "A coming-of-age novel following Holden Caulfield's journey through adolescence and alienation.",
+  },
+  {
+    bookName: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K. Rowling",
+    bookPublished: "June 26, 1997",
+    language: "English",
+    publisher: "Bloomsbury",
+    pageCount: 223,
+    shortInfo:
+      "The first book in the fantasy series about a young wizard, Harry Potter, and his adventures at Hogwarts School of Witchcraft and Wizardry.",
+  },
+  {
+    bookName: "Crime and Punishment",
+    author: "Fyodor Dostoevsky",
+    bookPublished: "January 1866",
+    language: "Russian",
+    publisher: "The Russian Messenger",
+    pageCount: 671,
+    shortInfo:
+      "A psychological novel delving into the inner turmoil of a young man, Raskolnikov, after committing a murder.",
+  },
+];
--- src/main.js
+++ src/main.js
@@ -1,19 +1,152 @@
-let myLibrary = [
-  {
-    bookName: "Atomic Havits",
-    author: "James Clear",
-    bookPublished: "October 16, 2018",
-    language: "English",
-    publisher: "Penguin Publishing Group",
-    pageCount: 320,
-    shortInfo: "An Easy & Proven Way to Build Good Habits & Break Bad Ones",
-  },
-];
+const inputBookName = document.getElementById("book-name");
+const booksContainer = document.getElementById("books-container");
+inputBookName.addEventListener("input", getBook);
+const formContainer = document.getElementById("form-container");
+const form = document.querySelector("form");
+const addBook = document.getElementById("add-book");
+const closeBtn = document.getElementById("closeBtn");
 
-function Book() {
-  console.log("hello");
+closeBtn.addEventListener("click", () => showForm(false));
+
+addBook.addEventListener("click", () => showForm(true));
+
+function showForm(e) {
+  console.log(e);
+  if (e) {
+    formContainer.style.display = "flex";
+    setTimeout(() => {
+      formContainer.style.opacity = 1;
+    }, 300);
+  } else {
+    formContainer.style.opacity = 0;
+    setTimeout(() => {
+      formContainer.style.display = "none";
+    }, 300);
+  }
+}
+
+function Book(updatedBook) {
+  for (let book in updatedBook) {
+    const details = document.createElement("div");
+    const bookTitle = document.createElement("div");
+    const info = document.createElement("div");
+    const auth = document.createElement("p");
+    const title = document.createElement("h1");
+    const year = document.createElement("p");
+    const count = document.createElement("p");
+    const short = document.createElement("p");
+    const pub = document.createElement("p");
+    const lang = document.createElement("p");
+    const btn = document.createElement("button");
+
+    let bookName = updatedBook[book].bookName;
+    let author = updatedBook[book].author;
+    let published = updatedBook[book].bookPublished;
+    let pageCount = updatedBook[book].pageCount;
+    let shortInfo = updatedBook[book].shortInfo;
+    let language = updatedBook[book].language;
+    let publisher = updatedBook[book].publisher;
+
+    title.textContent = bookName;
+    auth.textContent = `by ${author}`;
+    year.textContent = published;
+    count.textContent = `Pages: ${pageCount}`;
+    short.textContent = shortInfo;
+    pub.textContent = publisher;
+    lang.textContent = language;
+    btn.textContent = "Read Now";
+
+    bookTitle.appendChild(title);
+    bookTitle.appendChild(lang);
+    bookTitle.appendChild(auth);
+    bookTitle.appendChild(year);
+    bookTitle.appendChild(pub);
+    bookTitle.appendChild(count);
+
+    info.appendChild(short);
+    info.appendChild(btn);
+
+    details.appendChild(bookTitle);
+    details.appendChild(info);
+
+    short.className = "";
+    btn.className =
+      "border bg-green-600 text-slate-50 col-span-2 p-2 rounded-lg";
+    pub.className = "col-span-3";
+    lang.className = "text-right";
+    auth.className = "col-span-2";
+    short.className = "col-span-2";
+    count.className = "text-right";
+    year.className = "text-right col-span-2 ";
+    info.className = "grid grid-cols-2 text-justify";
+    title.className = "text-xl col-span-3";
+    bookTitle.className = "grid grid-cols-4 gap-1";
+    details.className =
+      "w-full h-full border p-4 gap-2 shadow-sm grid bg-slate-50";
+
+    booksContainer.appendChild(details);
+  }
+}
+
+function getBook(e) {
+  let name = e.target.value;
+  const filterBook = myLibrary.filter((book) => {
+    const nameofBook = book["bookName"]
+      .toLowerCase()
+      .includes(name.toLowerCase());
+    const authorofBook = book["author"]
+      .toLowerCase()
+      .includes(name.toLowerCase());
+    const yearofBook = book["bookPublished"]
+      .toLowerCase()
+      .includes(name.toLowerCase());
+    const infoDetails = book["shortInfo"]
+      .toLowerCase()
+      .includes(name.toLowerCase());
+    const language = book["language"]
+      .toLowerCase()
+      .includes(name.toLowerCase());
+
+    return nameofBook || authorofBook || yearofBook || infoDetails || language;
+  });
+  resetBookList();
+  if (filterBook.length === 0) {
+    return (booksContainer.textContent = "No Books Found");
+  } else {
+    return Book(filterBook);
+  }
+}
+
+function resetBookList() {
+  while (booksContainer.firstChild) {
+    booksContainer.removeChild(booksContainer.firstChild);
+  }
+}
+
+function addBookToLibrary() {
+  const formData = new FormData(form);
+  let entries = formData.entries();
+
+  let newbook = new Object();
+
+  for (let [name, value] of formData.entries()) {
+    newbook[name] = value;
+  }
+  const isDouble = myLibrary.some((book) => {
+    return (
+      book.bookName === newbook.bookName &&
+      book.author === newbook.author &&
+      book.pageCount === newbook.pageCount
+    );
+  });
+  if (isDouble) return false;
+  myLibrary.push(newbook);
+  resetBookList();
+  showForm(false);
+  Book(myLibrary);
+  return false;
 }
 
 window.onload = () => {
-  Book();
+  Book(myLibrary);
 };
